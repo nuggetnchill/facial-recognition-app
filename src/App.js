7ecfa92a81a1eafa28bf4b7bc3e46fde31092b96
nuggetnchill <nuggetnchill@gmail.com>
2020-06-30 20:52:51 -0500

    sign in feature added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 
@@ -38,6 +40,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box:[],
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -64,7 +68,6 @@ class App extends Component {
 
   displayFaceBox = (box) => {
     this.setState({box:box});
-    console.log(box)
   }
 
   onInputChange = (event) => {
@@ -81,21 +84,40 @@ class App extends Component {
     .catch(err => console.log(err));
   }
 
+  onRouteChange = (route) => {
+    if (route === 'signout'){
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route})
+  }
+
 render() {
+  
   return (
     <div className="App">
       <Particles className="particles"
         params={particlesOptions}
       />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm 
-      onInputChange={this.onInputChange} 
-      onButtonSubmit={this.onButtonSubmit}/>
-      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+      <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
+      { this.state.route === 'home' 
+        ? <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit} />
+            <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+          </div>
+         : (
+              this.state.route ==='signin'
+            ? <Signin onRouteChange={this.onRouteChange} />
+            : <Register onRouteChange={this.onRouteChange} />
+            ) 
+      }
     </div>
-  );
+  )
 }
 }
 
