b27f1d53773313159a34db45b1cd507a49c1adbc
Justin Nguyen <nuggetnchill@gmail.com>
2020-07-30 16:32:18 -0500

    added loading animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import Rank from './components/Rank/Rank';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import Signin from './components/Signin/Signin';
-import Register from './components/Register/Register';
-import Particles from 'react-particles-js';
+import React, { Component } from "react";
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import Rank from "./components/Rank/Rank";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
+import Particles from "react-particles-js";
+
+// (for loading animation)
+import { usePromiseTracker, trackPromise } from "react-promise-tracker";
+import Loader from "react-loader-spinner";
+
+const LoadingIndicator = (props) => {
+  const { promiseInProgress } = usePromiseTracker();
+  return (
+    promiseInProgress && (
+      <div
+        style={{
+          width: "100%",
+          height: "100",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Loader type='TailSpin' color='#fcfcfc' height='100' width='100' />
+      </div>
+    )
+  );
+};
 
 const particlesOptions = {
   particles: {
@@ -15,93 +38,93 @@ const particlesOptions = {
       shadow: {
         enable: true,
         color: "#000000",
-        blur: 2
-      }
-    },   
-    number:{
+        blur: 2,
+      },
+    },
+    number: {
       value: 30,
-      density: { 
+      density: {
         enable: true,
-        value_area: 800
-      }
-    }
-  }   
-}
+        value_area: 800,
+      },
+    },
+  },
+};
 
 const initialState = {
-  input: '',
-  imageUrl: '',
-  box:[],
-  route: 'signin',
+  input: "",
+  imageUrl: "",
+  box: [],
+  route: "signin",
   isSignedIn: false,
   user: {
-    id: '',
-    name: '',
-    email: '',
+    id: "",
+    name: "",
+    email: "",
     entries: 0,
-    joined: ''
-  }
-}
+    joined: "",
+  },
+};
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: '',
-      imageUrl: '',
-      box:[],
-      route: 'signin',
+      input: "",
+      imageUrl: "",
+      box: [],
+      route: "signin",
       isSignedIn: false,
       user: {
-        id: '',
-        name: '',
-        email: '',
+        id: "",
+        name: "",
+        email: "",
         entries: 0,
-        joined: ''
-      }
-    }
+        joined: "",
+      },
+    };
   }
 
   loadUser = (data) => {
-    this.setState({user: {
-      id: data.id,
-      name: data.name,
-      email: data.email,
-      entries: data.entries,
-      joined: data.joined
-    }})
-  }
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions
-      .map((box) => { 
-        return box.region_info.bounding_box
-      })
-    const image = document.getElementById('inputimage');
+    const clarifaiFace = data.outputs[0].data.regions.map((box) => {
+      return box.region_info.bounding_box;
+    });
+    const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
-    const box = clarifaiFace
-      .map((face) => {
-        return {
-          leftCol: face.left_col * width,
-          topRow: face.top_row * height,
-          rightCol: width - (face.right_col * width),
-          bottomRow: height - (face.bottom_row * height)
-        }
-      }
-    );
+    const box = clarifaiFace.map((face) => {
+      return {
+        leftCol: face.left_col * width,
+        topRow: face.top_row * height,
+        rightCol: width - face.right_col * width,
+        bottomRow: height - face.bottom_row * height,
+      };
+    });
     return box;
-  }
+  };
 
   displayFaceBox = (box) => {
-    this.setState({box:box});
-  }
+    this.setState({ box: box });
+  };
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
-  }
+    this.setState({ input: event.target.value });
+  };
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input})
+    this.setState({ imageUrl: this.state.input });
+    trackPromise(
       fetch("https://pure-mountain-22153.herokuapp.com/imageurl", {
         method: "post",
         headers: { "Content-Type": "application/json" },
@@ -129,44 +152,62 @@ class App extends Component {
           }
           this.displayFaceBox(this.calculateFaceLocation(response));
         })
-        .catch((err) => console.log(err));
-  }
+        .catch((err) => console.log(err))
+    );
+  };
 
   onRouteChange = (route) => {
-    if (route === 'signout'){
-      this.setState({initialState})
-    } else if (route === 'home') {
-      this.setState({isSignedIn: true})
+    if (route === "signout") {
+      this.setState({ initialState });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
     }
-    this.setState({route: route})
-  }
+    this.setState({ route: route });
+  };
 
-render() {
-  const { isSignedIn, imageUrl, route, box } = this.state;
-  return (
-    <div className="App">
-      <Particles className="particles"
-        params={particlesOptions}
-      />
-      <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
-      { route === 'home' 
-        ? <div>
+  render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
+    return (
+      <div className='App'>
+        <Particles className='particles' params={particlesOptions} />
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {route === "home" ? (
+          <div>
             <Logo />
-            <Rank name={this.state.user.name} entries={this.state.user.entries} />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
             <ImageLinkForm
               onInputChange={this.onInputChange}
-              onButtonSubmit={this.onButtonSubmit} />
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <LoadingIndicator />
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
-        : (
-              route ==='signin'
-            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-            ) 
-      }
-    </div>
-  )
-}
+        ) : route === "signin" ? (
+          <div>
+            <Signin
+              loadUser={this.loadUser}
+              onRouteChange={this.onRouteChange}
+            />
+            <LoadingIndicator />
+          </div>
+        ) : (
+          <div>
+            <Register
+              loadUser={this.loadUser}
+              onRouteChange={this.onRouteChange}
+            />
+            <LoadingIndicator />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
